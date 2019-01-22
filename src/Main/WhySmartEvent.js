088fb31668297a8f5a393c636025653f585635b7
gayanehakobyan <44981467+gayanehakobyan@users.noreply.github.com>
2019-01-22 12:38:16 +0400

    Update WhySmartEvent.js

--- src/Main/WhySmartEvent.js
+++ src/Main/WhySmartEvent.js
@@ -24,11 +24,11 @@ class WhySmartEvent extends Component {
         }
         console.log(this.state.mounted)
         return (
-            <div className='WhySmartEvent lg:flex md:flex sm:block  justife-center0'
+            <div className='WhySmartEvent lg:flex md:flex sm:block  justife-center'
             style={{opacity:opacityIndex,  heightIndex, transition:'opacity 4s ease-out'}}>
-                <div className='lg:w-1/4 text-grey-darker text-center  px-4 py-2 m-10 md:w-3/5  sm:w-2/5' style={{height: heightIndex, transition:'height 2s ease-out'}}> <h4>24/7 costomer service!!!</h4></div>
-                <div className='lg:w-1/4 text-grey-darker text-center  px-10 py-2 m-10 md:w-3/5 sm:w-2/5'  style={{height: heightIndex, transition:'height 2s ease-out'}}><h4>Our Live chat is the fastest way to engage your customers!!!</h4></div>
-                <div className='lg:w-1/4 text-grey-darker text-center  px-10 py-2 m-10 md:w-3/5 sm:w-2/5'  style={{height: heightIndex, transition:'height 2s ease-out'}}><h4>Talk to your customers in real-time!!! </h4></div>
+                <div className='w-3/5 inline-block text-grey-darker text-center  px-4 py-2 m-10 md:w-3/5  sm:w-2/5' style={{height: heightIndex, transition:'height 2s ease-out'}}> <h4>24/7 costomer service!!!</h4></div>
+                <div className='w-3/5 inline-block text-grey-darker text-center  px-10 py-2 m-10 md:w-3/5 sm:w-2/5 '  style={{height: heightIndex, transition:'height 2s ease-out'}}><h4>Our Live chat is the fastest way to engage your customers!!!</h4></div>
+                <div className='w-3/5 inline-block text-grey-darker text-center  px-10 py-2 m-10 md:w-3/5 sm:w-2/5'  style={{height: heightIndex, transition:'height 2s ease-out'}}><h4>Talk to your customers in real-time!!! </h4></div>
             </div>
         )
     }
